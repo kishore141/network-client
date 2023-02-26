e3c1cbc2cf1f1da17f85142fc3d073edd2c64916
kishore <kjkishore141@gmail.com>
2023-02-27 03:20:49 +0530

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,50 @@
+const axios = require("axios");
+
+const IS_NETWORK_REQUEST_LOGGING_ENABLED =
+  process.env.K_NETWORK_CLIENT_IS_REQUEST_LOGGING;
+const IS_NETWORK_RESPONSE_LOGGING_ENABLED =
+  process.env.K_NETWORK_CLIENT_IS_REQUEST_LOGGING;
+const IS_NETWORK_ERROR_LOGGING_ENABLED =
+  process.env.K_NETWORK_CLIENT_IS_REQUEST_LOGGING;
+
+const postJson = async (url, body, headers, log = true) => {
+  if (IS_NETWORK_REQUEST_LOGGING_ENABLED && log) {
+    console.log(JSON.stringify({ url, body, headers }));
+  }
+
+  try {
+    const { data, status, statusText, headers, config, request } =
+      await axios.post(url, body, {
+        headers: headers,
+      });
+    if (IS_NETWORK_RESPONSE_LOGGING_ENABLED && log) {
+      console.log(
+        JSON.stringify({
+          LOG_TYPE: "NETWORK RESPONSE",
+          STATUS: status,
+          STATUS_TEXT: statusText,
+          RESPONSE_DATA: data,
+        })
+      );
+    }
+    return { success: true, response: data, error: false };
+  } catch (ex) {
+    const { data, status, statusText, headers, config, request, message } = ex;
+    if (IS_NETWORK_ERROR_LOGGING_ENABLED && log) {
+      console.log(
+        JSON.stringify({
+          LOG_TYPE: "NETWORK ERROR",
+          STATUS: status,
+          STATUS_TEXT: statusText,
+          MESSAGE: message,
+          RESPONSE_DATA: data,
+        })
+      );
+    }
+    return { success: false, response: data, error: true };
+  }
+};
+
+module.exports = {
+  postJson,
+};
